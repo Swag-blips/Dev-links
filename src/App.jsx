a8e4a7c82b4f2aa04f0cb256f7af875e8d918aed
Swag-blips <coderblip@gmail.com>
2024-05-08 10:13:33 +0100

    first iteration link sharing app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-2xl">Hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
