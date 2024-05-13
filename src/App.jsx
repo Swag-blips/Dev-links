1397f92f436b3328ffc35077c0eac9420d946edc
Swag-blips <coderblip@gmail.com>
2024-05-13 11:26:08 +0100

    routing refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/auth/Login.jsx";
+import ErrorPage from "./pages/Error.jsx";
+import SignUp from "./pages/auth/SignUp.jsx";
+import HomePage from "./pages/home/HomePage.jsx";
+import PrivateRoute from "../routes/PrivateRoute.jsx";
+import PublicRoute from "../routes/PublicRoute.jsx";
+import Profile from "./pages/profile/Profile.jsx";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: (
+        <PublicRoute>
+          <Login />
+        </PublicRoute>
+      ),
+      error: <ErrorPage />,
+    },
+    {
+      path: "/signup",
+      element: (
+        <PublicRoute>
+          <SignUp />
+        </PublicRoute>
+      ),
+      error: <ErrorPage />,
+    },
+    {
+      path: "/",
+      element: (
+        <PrivateRoute>
+          <HomePage />
+        </PrivateRoute>
+      ),
+      error: <ErrorPage />,
+    },
+    {
+      path: "/profile/:id",
+      element: <Profile />,
+      error: <ErrorPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import Login from "./pages/auth/Login.jsx";
-import ErrorPage from "./pages/Error.jsx";
-import SignUp from "./pages/auth/SignUp.jsx";
-import HomePage from "./pages/home/HomePage.jsx";
-import PrivateRoute from "../routes/PrivateRoute.jsx";
+
 import { AuthProvider } from "../firebase/AuthContext.jsx";
-import PublicRoute from "../routes/PublicRoute.jsx";
-import Profile from "./pages/profile/Profile.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: (
-      <PublicRoute>
-        <Login />
-      </PublicRoute>
-    ),
-    error: <ErrorPage />,
-  },
-  {
-    path: "/signup",
-    element: (
-      <PublicRoute>
-        <SignUp />
-      </PublicRoute>
-    ),
-    error: <ErrorPage />,
-  },
-  {
-    path: "/",
-    element: (
-      <PrivateRoute>
-        <HomePage />
-      </PrivateRoute>
-    ),
-    error: <ErrorPage />,
-  },
-  {
-    path: "/profile/:id",
-    element: <Profile />,
-    error: <ErrorPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <Toaster />
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
